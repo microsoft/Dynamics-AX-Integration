9391c359720992561db8fa164d9e060fcf1d6bd7
nimak@microsoft.com <nimak@microsoft.com>
2016-02-08 14:03:57 -0800

    Removing binding limits from BasicHttpBinding.

--- ServiceSamples/SoapUtility/SoapHelper.cs
+++ ServiceSamples/SoapUtility/SoapHelper.cs
@@ -24,6 +24,27 @@ namespace SoapUtility
         {
             var binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
 
+            // Set binding timeout and other configuration settings
+            binding.ReaderQuotas.MaxStringContentLength = int.MaxValue;
+            binding.ReaderQuotas.MaxArrayLength = int.MaxValue;
+            binding.ReaderQuotas.MaxNameTableCharCount = int.MaxValue;
+
+            binding.ReceiveTimeout = TimeSpan.MaxValue;
+            binding.SendTimeout = TimeSpan.MaxValue;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+
+            var httpsTransportBindingElement = binding.CreateBindingElements().OfType<HttpsTransportBindingElement>().FirstOrDefault();
+            if (httpsTransportBindingElement != null)
+            {
+                httpsTransportBindingElement.MaxPendingAccepts = 10000; // Largest posible is 100000, otherwise throws
+            }
+
+            var httpTransportBindingElement = binding.CreateBindingElements().OfType<HttpTransportBindingElement>().FirstOrDefault();
+            if (httpTransportBindingElement != null)
+            {
+                httpTransportBindingElement.MaxPendingAccepts = 10000; // Largest posible is 100000, otherwise throws
+            }
+
             return binding;
         }
     }
