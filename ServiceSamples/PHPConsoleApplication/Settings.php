3b6bc1e181f1db600fcaef832134c30d6a886d84
XaaK <z_greenvoss@hotmail.com>
2017-01-23 16:51:40 -0800

    Adding PHP Examples

---
+++ ServiceSamples/PHPConsoleApplication/Settings.php
@@ -0,0 +1,11 @@
+﻿<?php
+	class Settings
+	{        
+		public static $appADResource = '';
+		public static $appTenantDomainName = '';
+		public static $appADClientId = '';
+		
+		public static $appUserID = '';    
+		public static $password = '';
+	}
+?>
