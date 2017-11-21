1df282b5e3746ad0e255d96088687f1e024adc20
Bhaarath Raguru <bhraguru@microsoft.com>
2017-11-21 15:56:31 -0800

    Set TLS version to 1.2 before making SOAP calls

--- ServiceSamples/JsonConsoleApplication/Program.cs
+++ ServiceSamples/JsonConsoleApplication/Program.cs
@@ -11,6 +11,15 @@ namespace OAuthXppConsoleApplication
 
         static void Main(string[] args)
         {
+            /* Helpful Note: 
+             * If you are targetting SOAP endpoints on Sandbox or Prod AX environemnts you MUST make sure to use TLS 1.2
+             * .NET 4.5 supports TLS 1.2 but it is not the default protocol. The below statement can set TLS version explicity.
+             * Note that this statement may not work in .NET 4.0, 3.0 or below.
+             * Also note that in .NET 4.6 and above TLS 1.2 is the default protocol.
+             */
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             var request = HttpWebRequest.Create(GetUserSessionOperationPath);
             request.Headers[OAuthHelper.OAuthHeader] = OAuthHelper.GetAuthenticationHeader();
             request.Method = "POST";
