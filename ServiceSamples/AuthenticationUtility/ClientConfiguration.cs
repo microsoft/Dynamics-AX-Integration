79ab6b26f0e5873873de4551a6ebddee63b0d0d3
nguhoa <34080196+nguhoa@users.noreply.github.com>
2017-11-30 11:03:08 -0800

    Update ClientConfiguration.cs

--- ServiceSamples/AuthenticationUtility/ClientConfiguration.cs
+++ ServiceSamples/AuthenticationUtility/ClientConfiguration.cs
@@ -14,17 +14,18 @@ namespace AuthenticationUtility
         {
             UriString = "https://usnconeboxax1aos.cloud.onebox.dynamics.com/",
             UserName = "tusr1@TAEOfficial.ccsctp.net",            
-            // Insert the correct password here for actual tests.
+            // Insert the correct password here for the actual test.
             Password = "",
 
             ActiveDirectoryResource = "https://usnconeboxax1aos.cloud.onebox.dynamics.com",
-            ActiveDirectoryTenant = "https://sts.windows-ppe.net/TAEOfficial.ccsctp.net",
+            ActiveDirectoryTenant = "https://login.windows-ppe.net/TAEOfficial.ccsctp.net",
             ActiveDirectoryClientAppId = "d8a9a121-b463-41f6-a86c-041272bdb340",
+            // Insert here the application secret when authenticate with AAD by the application
             ActiveDirectoryClientAppSecret = "",
 
-            // Change TLS version of HTTP request from client here
-            // Ex: TLSVersionOfHTTPRequest = "1.2"
-            // Leave it empty if want to use default version
+            // Change TLS version of HTTP request from the client here
+            // Ex: TLSVersion = "1.2"
+            // Leave it empty if want to use the default version
             TLSVersion = "",
         };
 
