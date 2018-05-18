96db44517cf570ee386b599a1763edf474850c8f
REDMOND\chwolf <chwolf@microsoft.com>
2018-05-17 22:31:59 -0700

    Small change

--- ServiceSamples/JsonConsoleApplication/Program.cs
+++ ServiceSamples/JsonConsoleApplication/Program.cs
@@ -21,7 +21,7 @@ namespace OAuthXppConsoleApplication
             string GetUserSessionOperationPath = string.Format("{0}{1}", ClientConfiguration.Default.UriString.TrimEnd('/'), sessionUrl);
             
             var request = HttpWebRequest.Create(GetUserSessionOperationPath);     
-            // If you call GetAuthenticationHeader with true you will follow auth vie AAD Web App, otherwise via Native AAD App
+            // If you call GetAuthenticationHeader with true you will the auth via AAD Web App, otherwise via Native AAD App
             request.Headers[OAuthHelper.OAuthHeader] = OAuthHelper.GetAuthenticationHeader();
             request.Method = "POST";
             request.ContentLength = 0;
