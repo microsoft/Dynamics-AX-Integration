70522270bf28e3fb17fb74d7b4d5b37b20fe591b
Christian Wolf <chwolf@microsoft.com>
2018-05-17 22:51:28 -0700

    Adding comments (#68)
    * Adding comments
    
    * Small change

--- ServiceSamples/AuthenticationUtility/ClientConfiguration.cs
+++ ServiceSamples/AuthenticationUtility/ClientConfiguration.cs
@@ -12,13 +12,16 @@ namespace AuthenticationUtility
 
         public static ClientConfiguration OneBox = new ClientConfiguration()
         {
+            // You only need to populate this section if you are logging on via a native app. For Service to Service scenarios in which you e.g. use a service principal you don't need that.
             UriString = "https://usnconeboxax1aos.cloud.onebox.dynamics.com/",
             UserName = "tusr1@TAEOfficial.ccsctp.net",            
             // Insert the correct password here for the actual test.
             Password = "",
 
-            ActiveDirectoryResource = "https://usnconeboxax1aos.cloud.onebox.dynamics.com",
-            ActiveDirectoryTenant = "https://login.windows-ppe.net/TAEOfficial.ccsctp.net",
+            // You need this only if you logon via service principal using a client secret. See: https://docs.microsoft.com/en-us/dynamics365/unified-operations/dev-itpro/data-entities/services-home-page to get more data on how to populate those fields.
+            // You can find that under AAD in the azure portal
+            ActiveDirectoryResource = "https://usnconeboxax1aos.cloud.onebox.dynamics.com", // Don't have a trailing "/". Note: Some of the sample code handles that issue.
+            ActiveDirectoryTenant = "https://login.windows-ppe.net/TAEOfficial.ccsctp.net", // Some samples: https://login.windows.net/yourtenant.onmicrosoft.com, https://login.windows.net/microsoft.com
             ActiveDirectoryClientAppId = "d8a9a121-b463-41f6-a86c-041272bdb340",
             // Insert here the application secret when authenticate with AAD by the application
             ActiveDirectoryClientAppSecret = "",
--- ServiceSamples/JsonConsoleApplication/Program.cs
+++ ServiceSamples/JsonConsoleApplication/Program.cs
@@ -13,13 +13,15 @@ namespace OAuthXppConsoleApplication
 {
     class Program
     {
+        // In the AOT you will find UserSessionService in Service Groups and AifUserSessionService under Services.
         public static string sessionUrl = "/api/services/UserSessionService/AifUserSessionService/GetUserSessionInfo";
         
         static void Main(string[] args)
         {
             string GetUserSessionOperationPath = string.Format("{0}{1}", ClientConfiguration.Default.UriString.TrimEnd('/'), sessionUrl);
             
-            var request = HttpWebRequest.Create(GetUserSessionOperationPath);            
+            var request = HttpWebRequest.Create(GetUserSessionOperationPath);     
+            // If you call GetAuthenticationHeader with true you will the auth via AAD Web App, otherwise via Native AAD App
             request.Headers[OAuthHelper.OAuthHeader] = OAuthHelper.GetAuthenticationHeader();
             request.Method = "POST";
             request.ContentLength = 0;
