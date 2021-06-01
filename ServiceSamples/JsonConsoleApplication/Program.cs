129d68dc28fa9b9248341b5599af6045c54bb2dc
Rolf Staflin <github@piro.se>
2021-06-01 16:47:33 +0200

    Clarified how to use AAD web app authentication.
    The sample was using ClientConfiguration.Default.UriString for the operation path, which was causing an error when calling OAuthHelper.GetAuthenticationHeader(true). This patch adds a boolean constant, UseAadWebAppAuthentication, and uses its value to set the correct operation path for the chosen authentication.

--- ServiceSamples/JsonConsoleApplication/Program.cs
+++ ServiceSamples/JsonConsoleApplication/Program.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Security.Authentication;
 using System.Text;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace OAuthXppConsoleApplication
 {
@@ -16,13 +17,20 @@ namespace OAuthXppConsoleApplication
         // In the AOT you will find UserSessionService in Service Groups and AifUserSessionService under Services.
         public static string sessionUrl = "/api/services/UserSessionService/AifUserSessionService/GetUserSessionInfo";
         
+        // Set to true to authenticate as an AAD Web App, or false to autenticate as a Native AAD App.
+        // See https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-app-types
+        private const bool UseAadWebAppAuthentication = false;
+
         static void Main(string[] args)
         {
-            string GetUserSessionOperationPath = string.Format("{0}{1}", ClientConfiguration.Default.UriString.TrimEnd('/'), sessionUrl);
+            var uriPrefix = UseAadWebAppAuthentication
+                ? ClientConfiguration.Default.ActiveDirectoryResource
+                : ClientConfiguration.Default.UriString;
+            string GetUserSessionOperationPath = string.Format("{0}{1}", uriPrefix.TrimEnd('/'), sessionUrl);
             
             var request = HttpWebRequest.Create(GetUserSessionOperationPath);     
             // If you call GetAuthenticationHeader with true you will the auth via AAD Web App, otherwise via Native AAD App
-            request.Headers[OAuthHelper.OAuthHeader] = OAuthHelper.GetAuthenticationHeader();
+            request.Headers[OAuthHelper.OAuthHeader] = OAuthHelper.GetAuthenticationHeader(UseAadWebAppAuthentication);
             request.Method = "POST";
             request.ContentLength = 0;
 
